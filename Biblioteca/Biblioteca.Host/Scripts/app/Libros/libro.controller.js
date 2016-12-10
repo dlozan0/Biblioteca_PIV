193433f2548116048dcb5de161e197354eabe4db
Laboratorio <Laboratorio>
2016-12-10 12:20:57 -0600

    falta la parte de libros

---
+++ Biblioteca/Biblioteca.Host/Scripts/app/Libros/libro.controller.js
@@ -0,0 +1,11 @@
+﻿//ha este controlador tenemos que hagregarle el editorialService para que pueda traer las editoriales de la BD
+//tambien agregaremso los siguiente justamente debajo de agregar 
+
+$scope.obtenerEditoriales = function () {
+    editorialService.obtenerEditoriales()
+    .then(function(response)){
+    $scope.editoriales = rsponse.data;
+
+
+}
+}
